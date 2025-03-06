fa05acc1c13ce8aab7a189e17cb977bb8ac26549
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2025-03-06 11:19:44 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,12 +12,14 @@ import path from "path";
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Frontend origin
+  res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const PORT = process.env.PORT || 3000;
 
 const __dirname = path.resolve();
