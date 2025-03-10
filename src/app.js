3969f2a859f2e1c946463a2be58445324625a001
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2025-03-10 16:19:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 //FIXED CORS CONFIGURATION
 app.use(cors({
-  origin: 'http://localhost:5173',  // Allow your frontend URL
+  origin: 'https://chatty-six-theta.vercel.app',  // Allow your frontend URL
   credentials: true                  // Allow cookies, tokens, etc.
 }));
 
